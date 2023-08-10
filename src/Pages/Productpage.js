a6856d2d2fd5beb8269391d055ea08a96281898f
Shivam <Shivamhits1972@gmail.com>
2023-08-10 14:12:04 +0530

    Added product page

---
+++ src/Pages/Productpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "../container/header/header.jsx"
+
+function Productpage() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default Productpage;
