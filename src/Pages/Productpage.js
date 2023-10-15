33023a0e58428d0e797bd4da37d2f297785212f2
Shivam Awasthi <shivamhits1972@gmail.com>
2023-10-15 16:55:39 +0530

    product page

--- src/Pages/Productpage.js
+++ src/Pages/Productpage.js
@@ -12,21 +12,35 @@ function Productpage() {
 
         <div className="product_first_container_1">
           <div className='product_Page_image'>
+
             <div className='product_image_secondary'>
               <img src={product} className='product_page_image2' alt="product" srcset="" />
               <img src={product} className='product_page_image3' alt="product" srcset="" />
               <img src={product} className='product_page_image4' alt="product" srcset="" />
               <img src={product} className='product_page_image5' alt="product" srcset="" />
             </div>
+
             <img src={product} className='product_page_image_main' alt="product" srcset="" />
+
           </div>
+
           <div className='product_details'>
             <h3>Pantop DSR</h3>
             <p>Aristo Pharmaceuticals Pvt Ltd</p>
           </div>
+
           <div className='product_price'>
-            <p>3rd part</p>
+
+            <div className='without_membershp'>
+              <p>Rs.150</h2>
+            </div>
+
+            <div className='with_membershp'>
+
+            </div>
+
           </div>
+
         </div>
         {/* second part for ads and description */}
         <div className='product_first_container_2'>
