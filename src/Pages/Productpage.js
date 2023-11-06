b25e15a2cf4f1df1b2abf435917651aead7ce3ae
Shivam Awasthi <shivamhits1972@gmail.com>
2023-11-06 06:29:35 +0530

    product page started editing

--- src/Pages/Productpage.js
+++ src/Pages/Productpage.js
@@ -1,47 +1,15 @@
 import React from 'react'
 import Header from "../container/header/header.jsx"
 import "./Productpage.css"
-import product from "../images/Pantop.jpg"
-
+import Productpage_upperHalf from "../container/Productpage_upperHalf/Productpage_upperHalf.jsx"
 
 function Productpage() {
   return (
     <div className='product_page_container'>
         <Header/>
         {/* First container for image , price and details */}
+        <Productpage_upperHalf/>
 
-        <div className="product_first_container_1">
-          <div className='product_Page_image'>
-
-            <div className='product_image_secondary'>
-              <img src={product} className='product_page_image2' alt="product" srcset="" />
-              <img src={product} className='product_page_image3' alt="product" srcset="" />
-              <img src={product} className='product_page_image4' alt="product" srcset="" />
-              <img src={product} className='product_page_image5' alt="product" srcset="" />
-            </div>
-
-            <img src={product} className='product_page_image_main' alt="product" srcset="" />
-
-          </div>
-
-          <div className='product_details'>
-            <h3>Pantop DSR</h3>
-            <p>Aristo Pharmaceuticals Pvt Ltd</p>
-          </div>
-
-          <div className='product_price'>
-
-            <div className='without_membershp'>
-              <p>Rs.150</p>
-            </div>
-
-            <div className='with_membershp'>
-
-            </div>
-
-          </div>
-
-        </div>
         {/* second part for ads and description */}
         <div className='product_first_container_2'>
           
---
+++ src/container/Productpage_upperHalf/Productpage_upperHalf.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import product from "../../images/Pantop.jpg"
+import "./Productpage_upperHalf.css"
+
+function Productpage_upperHalf() {
+  return (
+    <div className='productPage_upperHalf_container'>
+        {/* 1st part of product page */}
+        <div className='product_Page_image'>
+
+            <div className='product_image_secondary'>
+              <img src={product} className='product_page_image2' alt="product" srcset="" />
+              <img src={product} className='product_page_image3' alt="product" srcset="" />
+              <img src={product} className='product_page_image4' alt="product" srcset="" />
+              <img src={product} className='product_page_image5' alt="product" srcset="" />
+            </div>
+
+            <img src={product} className='product_page_image_main' alt="product" srcset="" />
+
+          </div>
+        {/* 2nd part of product page */}
+        <div className='product_details'>
+            <h3>Pantop DSR</h3>
+            <p>Aristo Pharmaceuticals Pvt Ltd</p>
+        </div>
+        {/* 3rd part */}
+        <div className='product_price'>
+            <div className='without_membershp'>
+              <p>Rs.150</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Productpage_upperHalf;
