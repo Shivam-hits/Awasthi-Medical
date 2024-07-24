b1291fdf669ca65e1e26006760a5d7aab971c741
Shivam Awasthi <shivamhits1972@gmail.com>
2024-07-24 09:14:19 +0530

    comments

--- src/container/ProductBrowser/productbrow.jsx
+++ src/container/ProductBrowser/productbrow.jsx
@@ -40,3 +40,34 @@ export default function Productbrowser({products}) {
     </>
   );
 }
+
+/*
+"Object.keys(util)"
+
+-) util is an object imported from product_details.js that contains multiple product arrays.
+-) "Object.keys(util)" returns an array of the keys (names of the properties) of the util object.
+-) For example, if util contains product1 and product2, Object.keys(util) would return ['product1', 'product2']. 
+
+
+".map((key) => ( "
+-) The map function iterates over each key in the array returned by Object.keys(util).
+-) For each key, it creates a new element, which in this case is a Productbrowser component.
+
+" <Productbrowser key={key} products={util[key]} /> "
+-) key={key}: React requires a unique key prop for each element in an array to keep track of them and optimize rendering. Using key={key} ensures each Productbrowser component has a unique key based on the property name (e.g., product1, product2).
+-) products={util[key]}: The products prop of the Productbrowser component is set to the corresponding product array from the util object.
+-) util[key] accesses the value of the property key in the util object. If key is product1, util[key] is util.product1.
+
+
+The map function iterates over this array:
+
+For the first iteration:
+-) key is 'product1'.
+-) A Productbrowser component is created with key='product1' and products={util['product1']} (which is the array of products in product1).
+For the second iteration:
+-) key is 'product2'.
+-) A Productbrowser component is created with key='product2' and products={util['product2']} (which is the array of products in product2).
+
+
+
+*/
